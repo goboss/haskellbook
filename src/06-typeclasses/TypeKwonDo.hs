4efb597ad1b41d8c470694dcaed650e761e4c2ce
Michał Wyrwalski <michal.wyrwalski@datarino.com>
2018-01-30 09:39:53 +0100

    06-typeclasses: complete chapter exercises

---
+++ src/06-typeclasses/TypeKwonDo.hs
@@ -0,0 +1,16 @@
+module TypeKwonDo where
+
+-- Exercise 1
+
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk f a b = f a == b
+
+-- Exercise 2
+
+arith :: Num b
+      => (a -> b)
+      -> Integer
+      -> a
+      -> b
+arith f i a = f a + (fromInteger i)
+
