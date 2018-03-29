df2fa5257205135ab1583ca49da4e7d342908757
Michał Wyrwalski <michal.wyrwalski@datarino.com>
2018-03-29 15:04:11 +0200

    24-parsers: add some more parsers for DOT

--- src/Exercises/Parsers/DOT.hs
+++ src/Exercises/Parsers/DOT.hs
@@ -4,8 +4,16 @@ import Data.Monoid ((<>))
 import Control.Applicative ((<|>))
 import Text.Trifecta
 
+-- Exercise
+-- Write a parser for the DOT language 14 that Graphviz uses to
+-- express graphs in plain-text (http://www.graphviz.org/doc/info/lang.html).
+
 data GraphType = Graph | Digraph deriving Show
 type GraphId = String
+type Attribute = (GraphId, GraphId)
+data Node = Node GraphId (Maybe Port) [Attribute] deriving Show
+data Compass = N | NE | E | SE | S | SW | W | NW | C | None deriving Show
+type Port = Either Compass (GraphId, (Maybe Compass))
 
 graphType :: Parser GraphType
 graphType =
@@ -13,8 +21,8 @@ graphType =
   <|>
   fmap (const Digraph) (string "digraph")
 
-graphId :: Parser (Maybe GraphId)
-graphId = optional (stringId <|> numeralId <|> quotedId <|> htmlId)
+graphId :: Parser GraphId
+graphId = stringId <|> numeralId <|> quotedId <|> htmlId
   where stringId :: Parser GraphId
         stringId =
           let beginning = char '_' <|> letter
@@ -33,3 +41,50 @@ graphId = optional (stringId <|> numeralId <|> quotedId <|> htmlId)
         htmlId :: Parser GraphId
         htmlId =
           between (char '<') (char '>') (many (noneOf "<>"))
+
+attr :: Parser Attribute
+attr = do
+  key <- token graphId
+  _   <- token (char '=')
+  val <- token graphId
+  return (key, val)
+
+attrList :: Parser [Attribute]
+attrList = open *> sepBy attr sep <* close
+  where open  = token (char '[')
+        close = token (char ']')
+        sep = token (char ',' <|> char ';')
+
+compass :: Parser Compass
+compass = choice
+  [
+    string "ne" *> (return NE),
+    string "n"  *> (return N),
+    string "se" *> (return SE),
+    string "e"  *> (return E),
+    string "sw" *> (return SW),
+    string "s"  *> (return S),
+    string "nw" *> (return NW),
+    string "w"  *> (return W),
+    string "c"  *> (return C),
+    string "_"  *> (return None)
+  ]
+
+port :: Parser Port
+port = fmap Left (try left) <|> fmap Right right
+  where left = sep *> compass
+        right = do
+          _ <- sep
+          i <- token graphId
+          c <- optional left
+          return (i, c)
+        sep = token (char ':')
+
+node :: Parser Node
+node = do
+  gi <- token graphId
+  po <- optional (token port)
+  as <- optional attrList
+  case as of
+    Just a  -> return (Node gi po a)
+    Nothing -> return (Node gi po [])
