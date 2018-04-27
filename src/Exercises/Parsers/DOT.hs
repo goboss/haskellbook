c3f1956f95f59c66891e40b3c74e7bef1a62eaaf
Michał Wyrwalski <michal.wyrwalski@datarino.com>
2018-04-27 11:01:39 +0200

    23-parsing: finish parsing DOT - the final exercise in the chapter

--- src/Exercises/Parsers/DOT.hs
+++ src/Exercises/Parsers/DOT.hs
@@ -1,71 +1,72 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Exercises.Parsers.DOT where
 
 import Data.Monoid ((<>))
 import Control.Applicative ((<|>), liftA2)
 import Text.Trifecta
+import Text.RawString.QQ
 
 -- Exercise
 -- Write a parser for the DOT language that Graphviz uses to
 -- express graphs in plain-text (http://www.graphviz.org/doc/info/lang.html).
 
-data GraphType = Graph | Digraph | Subgraph deriving Show
-type GraphId = String
+type ID = String
 
-type Attribute = (GraphId, GraphId)
-
-data Compass = N | NE | E | SE | S | SW | W | NW | C | None deriving Show
-type Port = Either Compass (GraphId, (Maybe Compass))
-
-type NodeId = (GraphId, Maybe Port)
-
-data Node = Node NodeId [Attribute] deriving Show
-
-data EdgeRHS = Directed NodeId | Undirected NodeId deriving Show
-data Edge = Edge NodeId [EdgeRHS] [Attribute] deriving Show
-
-type Assign = (GraphId, GraphId)
-
-graphType :: Parser GraphType
-graphType =
-  fmap (const Graph) (string "graph")
-  <|>
-  fmap (const Digraph) (string "digraph")
-  <|>
-  fmap (const Subgraph) (string "subgraph")
-
-graphId :: Parser GraphId
+graphId :: Parser ID
 graphId = token (stringId <|> numeralId <|> quotedId <|> htmlId)
-  where stringId :: Parser GraphId
+  where stringId :: Parser ID
         stringId =
           let beginning = char '_' <|> letter
               middle    = beginning <|> digit
           in some beginning <> many middle
-        numeralId :: Parser GraphId
+        numeralId :: Parser ID
         numeralId =
           let e2str (Left i)  = show i
               e2str (Right d) = show d
           in fmap e2str integerOrDouble
-        quotedId :: Parser GraphId
+        quotedId :: Parser ID
         quotedId =
           let escapedChar = (char '\\') *> (oneOf ['\\', '"'])
               normalChar = noneOf "\""
           in between (char '"') (char '"') (many (escapedChar <|> normalChar))
-        htmlId :: Parser GraphId
+        htmlId :: Parser ID
         htmlId =
           between (char '<') (char '>') (many (noneOf "<>"))
 
-attr :: Parser Attribute
-attr = do
-  key <- token graphId
-  _   <- token (char '=')
-  val <- token graphId
-  return (key, val)
+type Attr = (ID, ID)
+data Attribute =
+    GraphAttribute [Attr]
+  | NodeAttribute [Attr]
+  | EdgeAttribute [Attr]
+  deriving Show
 
-attrList :: Parser [Attribute]
-attrList = open *> sepBy attr sep <* close
-  where open  = token (char '[')
+attr :: Parser Attr
+attr = token (liftA2 (,) graphId (token (char '=') *> graphId))
+
+attrList :: Parser [Attr]
+attrList = fmap concat (many lst)
+  where lst = open *> sepBy attr sep <* close
+        open  = token (char '[')
         close = token (char ']')
         sep = token (char ',' <|> char ';')
+
+attribute :: Parser Attribute
+attribute = graphAttr <|> nodeAttr <|> edgeAttr
+  where graphAttr :: Parser Attribute
+        graphAttr = mkAttr "graph" GraphAttribute
+        nodeAttr :: Parser Attribute
+        nodeAttr = mkAttr "node" NodeAttribute
+        edgeAttr :: Parser Attribute
+        edgeAttr = mkAttr "edge" EdgeAttribute
+        mkAttr :: String -> ([Attr] -> Attribute) -> Parser Attribute
+        mkAttr name cons = fmap cons (token (string name) *> attrList)
+
+data Compass = N | NE | E | SE | S | SW | W | NW | C | None deriving Show
+type Port = Either Compass (ID, (Maybe Compass))
+
+type NodeId = (ID, Maybe Port)
+data Node = Node NodeId [Attr] deriving Show
 
 compass :: Parser Compass
 compass = choice
@@ -103,6 +104,9 @@ node = do
     Just a  -> return (Node ni a)
     Nothing -> return (Node ni [])
 
+data EdgeRHS = Directed NodeId | Undirected NodeId deriving Show
+data Edge = Edge NodeId [EdgeRHS] [Attr] deriving Show
+
 edgeRHS :: Parser EdgeRHS
 edgeRHS =
   fmap Directed (token (string "->") *> nodeId)
@@ -112,17 +116,38 @@ edgeRHS =
 edge :: Parser Edge
 edge = do
   eid <- nodeId
-  rhs <- many edgeRHS
+  rhs <- some edgeRHS
   att <- optional attrList
   case att of
     Just a  -> return (Edge eid rhs a)
     Nothing -> return (Edge eid rhs [])
 
-assign :: Parser Assign
-assign = token (liftA2 (,) graphId (token (char '=') *> graphId))
+data Statement =
+    NodeStmt Node
+  | EdgeStmt Edge
+  | AttributeStmt Attribute
+  | AttrStmt Attr
+  | SubgraphStmt Graph
+  deriving Show
 
-strict :: Parser ()
-strict = token (optional (string "strict")) *> return ()
+nodeStmt :: Parser Statement
+nodeStmt = fmap NodeStmt node
+
+edgeStmt :: Parser Statement
+edgeStmt = fmap EdgeStmt edge
+
+attributeStmt :: Parser Statement
+attributeStmt = fmap AttributeStmt attribute
+
+attrStmt :: Parser Statement
+attrStmt = fmap AttrStmt attr
+
+subgraphStmt :: Parser Statement
+subgraphStmt = do
+  g <- graph
+  case getType g of
+    SubgraphType -> return (SubgraphStmt g)
+    _            -> fail "expected: subgraph"
 
 comment :: Parser String
 comment =
@@ -135,3 +160,103 @@ comment =
     multi = string "/*" *> manyTill anyChar (try (string "*/"))
   in
     single <|> multi
+
+
+statement :: Parser Statement
+statement =
+  (
+    try attrStmt
+    <|>
+    try attributeStmt
+    <|>
+    try edgeStmt
+    <|>
+    try subgraphStmt
+    <|>
+    nodeStmt
+  ) <* many (token (string ";" <|> comment))
+
+data GraphType =
+    GraphType
+  | DigraphType
+  | StrictGraphType
+  | StrictDigraphType
+  | SubgraphType
+  deriving Show
+
+graphType :: Parser GraphType
+graphType = token $
+  fmap (const GraphType)
+    (string "graph")
+  <|>
+  fmap (const DigraphType)
+    (string "digraph")
+  <|>
+  fmap (const SubgraphType)
+    (string "subgraph")
+  <|>
+  fmap (const StrictGraphType)
+    (try (token (string "strict") >> string "graph"))
+  <|>
+  fmap (const StrictDigraphType)
+    (try (token (string "strict") >> string "digraph"))
+
+type Subgraph = Graph
+type SelfAttr = Attr
+
+data Graph =
+  Graph {
+      getType :: GraphType
+    , getId :: (Maybe ID)
+    , getNodes :: [Node]
+    , getEdges :: [Edge]
+    , getAttributes :: [Attribute]
+    , getSelfAttributes :: [SelfAttr]
+    , getSubgraph :: [Subgraph]
+  }
+  deriving (Show)
+
+graph :: Parser Graph
+graph = do
+  _   <- skipNoise
+  tpe <- graphType
+  gid <- optional graphId
+  _   <- token (char '{')
+  sts <- many statement
+  _   <- token (char '}')
+  _   <- skipNoise
+  let (ns, es, as, ss, gs) = pick (reverse sts) ([],[],[],[],[])
+  return (Graph tpe gid ns es as ss gs)
+    where skipNoise = skipMany (space <|> (comment >> return '\n'))
+          pick [] acc = acc
+          pick (x:xs) (ns, es, as, ss, gs) =
+            pick xs $ case x of
+              (NodeStmt n)       -> (n:ns, es, as, ss, gs)
+              (EdgeStmt e)       -> (ns, e:es, as, ss, gs)
+              (AttributeStmt a ) -> (ns, es, a:as, ss, gs)
+              (AttrStmt s)       -> (ns, es, as, s:ss, gs)
+              (SubgraphStmt g)   -> (ns, es, as, ss, g:gs)
+
+example :: String
+example = [r|
+// This is a test Graph
+ strict digraph {
+  awesome = yes # no rly!
+  node [test = passed];
+  graph [a = 1, b = 2];
+  edge [a = important, b = "also important"]
+  a -- b
+  c -> d
+  /* here we have
+   * a multiline comment
+   */
+  n1 [color = red]
+  d -> a
+  subgraph test {
+    g -- o -- o -- d
+  }
+}
+/* The End.
+ * Bye now!
+ */
+|]
